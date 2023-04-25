c0020bd69198a983ee5ba9f3040029dc3b9bfc22
Viktor <viktor.hidirov@yandex.ru>
2023-04-25 22:29:22 +0700

    feat: контроллер login

---
+++ controllers/login.js
@@ -0,0 +1,42 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
+const checkInfo = (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Not User'));
+      }
+      return bcrypt.compare(password, user.password);
+    })
+    .then((matched) => {
+      if (!matched) {
+        Promise.reject(new Error('Not User'));
+      } res.send({ message: 'OK' });
+    })
+    .catch((err) => {
+      res.status(401).send({ message: err.message });
+    });
+};
+
+const login = (req, res) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: "d285e3dceed844f902650f40" }, 'some-secret-key', { expiresIn: '7d' });
+      res.send({ token });
+    })
+    .catch((err) => {
+      res
+        .status(401)
+        .send({ message: err.message });
+    });
+};
+
+module.exports = {
+  checkInfo,
+  login,
+};
